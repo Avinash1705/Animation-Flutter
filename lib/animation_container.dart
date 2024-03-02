ff9c8d94d9fbfdccbc4c0f1f9c50de13c9c54a8a
Avinash <34656820+Avinash1705@users.noreply.github.com>
2024-03-03 00:12:04 +0530

    Tween (for auto animation)

--- lib/animation_container.dart
+++ lib/animation_container.dart
@@ -14,22 +14,22 @@ class _MyAnimationState extends State<MyAnimation> {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: Duration(seconds: 2),
-      width: _width,
-      margin: EdgeInsets.only(top: _topMargin),
-      color: Colors.lightBlueAccent,
-      child: ElevatedButton(
-        onPressed: () {
-          setState(() {
-            _width = 100;
-            _topMargin = 200;
-            _opacity = 0.5;
-          });
+    return TweenAnimationBuilder(
+        duration: Duration(seconds: 2),
+        tween: Tween<double>(begin: 0,end: 100),
+        builder: (BuildContext context, double value, Widget? child) {
+          return Container(
+              margin: EdgeInsets.only(left: value),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              
+            ),
+            child: Opacity(opacity: 1,child: Padding(
+              padding: EdgeInsets.all(value),
+              child:child ,
+            ),),
+          );
         },
-        child: Opacity(opacity: _opacity,
-        child: Center(child: Text("Press to see magic"))),
-      ),
-    );
+        child: Center(child: Text("Press to see magic",overflow: TextOverflow.ellipsis,style: TextStyle(color: Colors.red,fontSize:25),)));
   }
 }
